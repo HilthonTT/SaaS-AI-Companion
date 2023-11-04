6f8164009b1a2d0698399829eb4fed3a8afea60e
Timothy Tandt <Timothy.tandt@outlook.be>
2023-11-04 12:22:06 +0100

    [Feature] Category FIltering
    - Connect to planetscale using Prisma
    - Creates the category using seed script
    - Added the categories components for filtering

--- ai-companion/app/(root)/(routes)/page.tsx
+++ ai-companion/app/(root)/(routes)/page.tsx
@@ -1,9 +1,15 @@
+import prismaDb from "@/lib/prismadb";
+
+import { Categories } from "@/components/categories";
 import { SearchInput } from "@/components/search-input";
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prismaDb.category.findMany();
+
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 };
