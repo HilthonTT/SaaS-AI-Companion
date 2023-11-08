e6f2eb7c0d5c3aeec1c974dcfdf9fb7ce596bb85
Timothy Tandt <Timothy.tandt@outlook.be>
2023-11-08 15:58:16 +0100

    Fixed companion-form.tsx
    Replaced ` with &apos;

--- ai-companion/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ ai-companion/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -250,8 +250,8 @@ export const CompanionForm = ({
                   />
                 </FormControl>
                 <FormDescription>
-                  Describe in detail your companion's backstory and relevant
-                  details.
+                  Describe in detail your companion&apos;s backstory and
+                  relevant details.
                 </FormDescription>
                 <FormMessage />
               </FormItem>
